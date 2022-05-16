71fc023c9ebe8f7c1c27599c5fd729ecadd51fa2
igoor1 <iggoncalves1@outlook.com>
2022-05-16 17:51:51 -0300

    first

---
+++ filaBD/DAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace filaBD
+{
+    class DAO
+    {
+        string cpf, nome, sexo, idade, endereco;
+
+
+        public void cadastrar()
+        {
+            Console.WriteLine("cpf:");
+            cpf = Console.ReadLine();
+
+            Console.WriteLine("nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("sexo:");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("idade:");
+            idade = Console.ReadLine();
+                        Console.WriteLine("nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("endereco:");
+            endereco = Console.ReadLine();
+
+        }
+
+        public void insert()
+        {
+            String sql = "insert into paciente values(null,@cpf,@nome,@sexo,@idade,@endereco)";
+            cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@sexo",sexo);
+            cmd.Parameters.AddWithValue("@idade",idade);
+            cmd.Parameters.AddWithValue("@endereco", endereco);
+        }
+
+
+    }
+}
---
+++ filaBD/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace filaBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conexao = new MySqlConnection("server=localhost;database=fila;uid=root;password=root");
+            try
+            {
+                conexao.Open();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("erro");
+            }
+
+            DAO Daoo = new DAO();
+            
+            MySqlCommand cmd = new MySqlCommand(sql, conexao);
+
+            Daoo.cadastrar();
+            Daoo.insert();
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("cadastro concluido");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
